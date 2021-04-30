01cd2531f119f7aeda5fa916dd34ec57225fd40b
Limisiewicz Tomasz Ing <792-limisiet@users.noreply.localhost>
2021-04-30 13:15:24 +0200

    Reporting per languages UAS and UUAS for multiple languages trained jointly (e.g. en+es+zh)

--- src/reporting/reporter.py
+++ src/reporting/reporter.py
@@ -124,58 +124,60 @@ class UASReporter(Reporter):
     
     def write(self, args):
         for language in self._languages:
-            prefix = '{}.{}.'.format(self.dataset_name, language)
-            if self._probe_threshold:
-                prefix += 'gated.'
-                if self._drop_parts:
-                    prefix += 'dp{}.'.format(self._drop_parts)
-            if self.depths:
-                with open(os.path.join(args.out_dir, prefix + 'uas'), 'w') as uas_f:
-                    uas_f.write(str(self.uas[language].result())+'\n')
-            else:
-                with open(os.path.join(args.out_dir, prefix + 'uuas'), 'w') as uuas_f:
-                    uuas_f.write(str(self.uas[language].result())+'\n')
+            for lang in language.split('+'):
+                prefix = '{}.{}.'.format(self.dataset_name, lang)
+                if self._probe_threshold:
+                    prefix += 'gated.'
+                    if self._drop_parts:
+                        prefix += 'dp{}.'.format(self._drop_parts)
+                if self.depths:
+                    with open(os.path.join(args.out_dir, prefix + 'uas'), 'w') as uas_f:
+                        uas_f.write(str(self.uas[lang].result())+'\n')
+                else:
+                    with open(os.path.join(args.out_dir, prefix + 'uuas'), 'w') as uuas_f:
+                        uuas_f.write(str(self.uas[lang].result())+'\n')
     
     def compute(self, args):
         
         for language in self._languages:
-            self.uas[language] = UAS()
-            for conll_indices, num_tokens, pred_values, gold_values, mask in self.predict(args, language, language, 'dep_distance'):
+            for lang in language.split('+'):
+                self.uas[lang] = UAS()
+                for conll_indices, num_tokens, pred_values, gold_values, mask in self.predict(args, language, lang, 'dep_distance'):
                 
-                for conll_idx, sent_predicted, sent_gold, sent_len in zip(conll_indices.numpy(), pred_values, gold_values, num_tokens):
-                    sent_punctuation_mask = self.punctuation_masks[language][conll_idx]
-                    if self.depths:
-                        predicted_depths = self.depths[language][conll_idx]["predicted"]
-                        gold_depths = self.depths[language][conll_idx]["gold"]
-                        predicted_root = np.argmin(predicted_depths) + 1
-                        gold_root = np.argmin(gold_depths) + 1
+                    for conll_idx, sent_predicted, sent_gold, sent_len in zip(conll_indices.numpy(), pred_values, gold_values, num_tokens):
+                        sent_punctuation_mask = self.punctuation_masks[lang][conll_idx]
+                        if self.depths:
+                            predicted_depths = self.depths[lang][conll_idx]["predicted"]
+                            gold_depths = self.depths[lang][conll_idx]["gold"]
+                            predicted_root = np.argmin(predicted_depths) + 1
+                            gold_root = np.argmin(gold_depths) + 1
                     
-                    for i in range(sent_len):
-                        for j in range(sent_len):
-                            if sent_punctuation_mask[i] or sent_punctuation_mask[j]:
-                                sent_predicted[i, j] = np.inf
-                                sent_gold[i, j] = np.inf
-                            elif self.depths is None:
-                                if i > j:
+                        for i in range(sent_len):
+                            for j in range(sent_len):
+                                if sent_punctuation_mask[i] or sent_punctuation_mask[j]:
                                     sent_predicted[i, j] = np.inf
                                     sent_gold[i, j] = np.inf
-                            else:
-                                if predicted_depths[i] > predicted_depths[j]:
-                                    sent_predicted[i, j] = np.inf
-                                if gold_depths[i] > gold_depths[j]:
-                                    sent_gold[i, j] = np.inf
+                                elif self.depths is None:
+                                    if i > j:
+                                        sent_predicted[i, j] = np.inf
+                                        sent_gold[i, j] = np.inf
+                                else:
+                                    if predicted_depths[i] > predicted_depths[j]:
+                                        sent_predicted[i, j] = np.inf
+                                    if gold_depths[i] > gold_depths[j]:
+                                        sent_gold[i, j] = np.inf
                     
-                    min_spanning_tree = sparse.csgraph.minimum_spanning_tree(sent_predicted).tocoo()
-                    min_spanning_tree_gold = sparse.csgraph.minimum_spanning_tree(sent_gold).tocoo()
+                        min_spanning_tree = sparse.csgraph.minimum_spanning_tree(sent_predicted).tocoo()
+                        min_spanning_tree_gold = sparse.csgraph.minimum_spanning_tree(sent_gold).tocoo()
                     
-                    predicted = set(map(tuple, zip(min_spanning_tree.col + 1, min_spanning_tree.row + 1)))
-                    gold = set(map(tuple, zip(min_spanning_tree_gold.col + 1, min_spanning_tree_gold.row + 1)))
+                        predicted = set(map(tuple, zip(min_spanning_tree.col + 1, min_spanning_tree.row + 1)))
+                        gold = set(map(tuple, zip(min_spanning_tree_gold.col + 1, min_spanning_tree_gold.row + 1)))
                     
-                    if self.depths:
-                        predicted.add((predicted_root, 0))
-                        gold.add((gold_root, 0))
+                        if self.depths:
+                            predicted.add((predicted_root, 0))
+                            gold.add((gold_root, 0))
                     
-                    self.uas[language].update_state(gold, predicted)
+                        self.uas[lang].update_state(gold, predicted)
 
 
 class DependencyDepthReporter(Reporter):
@@ -184,11 +186,12 @@ class DependencyDepthReporter(Reporter):
         super().__init__(args, network, dataset, dataset_name)
     
     def compute(self, args):
-        results = {lang: dict() for lang in args.languages}
+        results = defaultdict(dict) # {lang: dict() for lang in args.languages}
         for language in args.languages:
-            for conll_indices, num_tokens, pred_values, gold_values, mask in self.predict(args, language, language, 'dep_depth'):
-                for conll_idx, sent_predicted, sent_gold in zip(conll_indices.numpy(), pred_values, gold_values):
-                    results[language][conll_idx] = {'predicted': sent_predicted, 'gold': sent_gold}
+            for lang in language.split('+'):
+                for conll_indices, num_tokens, pred_values, gold_values, mask in self.predict(args, language, lang, 'dep_depth'):
+                    for conll_idx, sent_predicted, sent_gold in zip(conll_indices.numpy(), pred_values, gold_values):
+                        results[lang][conll_idx] = {'predicted': sent_predicted, 'gold': sent_gold}
         
         return results
 
